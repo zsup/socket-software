1b752d42f4febdce53e8527548b3ef5f515d3182
Zach Supalla <zsupalla@gmail.com>
2012-04-22 16:09:53 -0500

    Cleaned up code for devices

--- device_led/device_led.ino
+++ device_led/device_led.ino
@@ -1,36 +1,47 @@
 
 // Arduino code for a SWITCH-enabled LED.
 // Communicates over serial connection (in practice, an XBee transmitter).
-// Available commands: 0-100
-// 0 = off
-// 1-99 = various dim levels
-// 100 = on
+// Available commands:
+//   turnOn: Turns lamp on
+//   turnOff: Turns lamp off
+//   toggle: Toggles lamp
+//   getStatus: Returns status of device in JSON
+//   dimX, where X is a number from 1 to 255; 
 //
 
 #include <String.h>
 
-#define BUFF_LEN 80
-#define DEVICE_ID "Elroy"
+#define BUFF_LEN 255
+#define DEVICE_ID "Astro"
 #define DEVICE_TYPE "LED"
 
-const int ledPin = 13; // the pin that the LED is attached to
-int status = 0; // status of the device
+const int ledPin = 9; // the pin that the LED is attached to
+int deviceStatus = 0; // deviceStatus of the device
+int dimLevel = 255; // dim level of the device
 
 char bufferString[BUFF_LEN+1]; // string to hold the text from the server
 int bufferLength = 0;
 
 void setup() {
-  
+
   // initialize serial communication:
   Serial.begin(9600);
-  
+
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
+
+  // Announce thyself
+  Serial.print("{ \"deviceid\" : \"");
+  Serial.print(DEVICE_ID);
+  Serial.print("\" , \"devicetype\" : \"");
+  Serial.print(DEVICE_TYPE);
+  Serial.print("\" , \"devicestatus\" : ");
+  Serial.print(deviceStatus);
+  Serial.println(" }");
 }
 
 void loop() {
-  // as long as there are bytes in the serial queue, 
-  // read them and send them out the socket
+  // as long as there are bytes in the serial queue, read them
   while (Serial.available() > 0) {
     char c = Serial.read();
 
@@ -47,40 +58,84 @@ void loop() {
 }
 
 void process() {
-  
-  // if the current line ends with turnOn, activate the light
-  if (strcmp(bufferString, "turnOn") == 0) {
-    digitalWrite(ledPin, HIGH);
-    status = 1;
-    Serial.println("On");
-  }
-    
-  // if the current line ends with turnOff, deactivate the light
-  else if (strcmp(bufferString, "turnOff") == 0) {
-    digitalWrite(ledPin, LOW);
-    status = 0;
-    Serial.println("Off");
-  }
-  
-  // if the current line ends with "toggle", toggle the light
-  else if (strcmp(bufferString, "toggle") == 0) {
-    if ( status == 1 ) {
-      digitalWrite(ledPin, LOW);
-      status = 0;
-    }
-    else {
-      digitalWrite(ledPin, HIGH);
-      status = 1;
-    }
-    Serial.println("Toggle.");
-  }
-  
-  else {
-    Serial.println("Unrecognized command.");
-  }
-  
-  // Erase the buffer string
   int i;
+
+  for ( i = 0 ; i < BUFF_LEN && bufferString[i] != ',' ; i++) {
+  }
+
+  if ( i == BUFF_LEN ) {
+    //Serial.println("Error; incorrect syntax, no comma found");
+  }
+
+  else if (strncmp(bufferString, DEVICE_ID, i) == 0) {
+    i++;
+    char* command = bufferString+i;
+
+    //Serial.println("This is for me! I'll do something with it.");
+    //Serial.print("Command: ");
+    //Serial.println(command);
+
+    // if the command is turnOn, activate the light
+    if (strcmp(command, "turnOn") == 0) {
+      analogWrite(ledPin, dimLevel);
+      deviceStatus = 1;
+      //Serial.println("On");
+    }
+
+    // if the command is turnOff, deactivate the light
+    else if (strcmp(command, "turnOff") == 0) {
+      digitalWrite(ledPin, LOW);
+      deviceStatus = 0;
+      //Serial.println("Off");
+    }
+
+    // if the command is "toggle", toggle the light
+    else if (strcmp(command, "toggle") == 0) {
+      if ( deviceStatus == 1 ) {
+        digitalWrite(ledPin, LOW);
+        deviceStatus = 0;
+      }
+      else {
+        analogWrite(ledPin, dimLevel);
+        deviceStatus = 1;
+      }
+      //Serial.println("Toggle.");
+    }
+    
+    // 
+    else if (strncmp(command, "dim", 3) == 0) {
+      char* endptr;
+      command = command + 3;
+      dimLevel = strtol (command, &endptr, 10);
+      if ( dimLevel > 0 && dimLevel < 256 ) {
+        analogWrite(ledPin, dimLevel);
+      }
+      else {
+        //Serial.println("Error: Bad dim level");
+      }
+      //Serial.print("Dim level: ");
+      //Serial.println(dimLevel);
+    }
+      
+
+    // if the command is "getStatus", respond with deviceStatus
+    else if (strcmp(command, "getStatus") == 0) {
+      Serial.print("{ \"deviceid\" : \"");
+      Serial.print(DEVICE_ID);
+      Serial.print("\" , \"devicestatus\" : ");
+      Serial.print(deviceStatus);
+      Serial.println(" }");
+    }
+
+    else {
+      //Serial.println("Unrecognized command.");
+    }
+  }
+  else {
+    //Serial.println("Not for me.");
+  }
+
+  // Erase the buffer string
   for (i = 0; i < BUFF_LEN; i++)
     bufferString[i] = '\0';
   bufferLength = 0;
