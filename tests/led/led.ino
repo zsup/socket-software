df57bd6c997061176c9857e5ba2a193ae4b5b119
Zach Supalla <zsupalla@gmail.com>
2012-10-06 21:09:24 -0500

    Changed LED test because LEDs are on at LOW (not HIGH)

--- tests/led/led.ino
+++ tests/led/led.ino
@@ -1,30 +1,58 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
+  LED test.
+  
+  Tests the LEDs for the Spark prototype.
+  
+  Note that the LEDs are on when they are pulled low, because they share a common cathode.
  */
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int led1 = 5;
-int led2 = 6;
-int led3 = 9;
+int red = 5;
+int blue = 6;
+int green = 9;
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(green, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 
 void loop() {
-  analogWrite(led1, 100);
+  digitalWrite(red, HIGH);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, HIGH);
   delay(1000);
-  analogWrite(led1, 255);
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, HIGH);
   delay(1000);
-  analogWrite(led1, 0);
+  digitalWrite(red, HIGH);
+  digitalWrite(green, LOW);
+  digitalWrite(blue, HIGH);
+  delay(1000);
+  digitalWrite(red, HIGH);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, LOW);
+  delay(1000);
+  digitalWrite(red, LOW);
+  digitalWrite(green, LOW);
+  digitalWrite(blue, HIGH);
+  delay(1000);
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, LOW);
+  delay(1000);
+  digitalWrite(red, HIGH);
+  digitalWrite(green, LOW);
+  digitalWrite(blue, LOW);
+  delay(1000);
+  digitalWrite(red, LOW);
+  digitalWrite(green, LOW);
+  digitalWrite(blue, LOW);
   delay(1000);
 }
